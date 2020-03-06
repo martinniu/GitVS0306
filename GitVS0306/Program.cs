7562918c836efdcf987da0d0eed217c65e51ac86
martinniu <martinniu@outlook.com>
2020-03-06 23:59:58 +0800

    first commit

---
+++ GitVS0306/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitVS0306
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello word");
+        }
+    }
+}
